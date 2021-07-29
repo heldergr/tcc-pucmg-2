902c16f8cb1b00b8c23be461179d163c6be2ca2c
Helder Ribeiro <helder.ribeiro@serpro.gov.br>
2021-07-29 09:04:14 -0300

    Documentacao resultados

--- src/python/notebooks/resultados/resultados_explorer.py
+++ src/python/notebooks/resultados/resultados_explorer.py
@@ -1,6 +1,7 @@
 from repository.resultado import ResultadoRepo
 
 import pandas as pd
+import seaborn as sns
 
 resultado_repo_wp_full = ResultadoRepo(collection_name='resultados-wp-full')
 resultado_repo_wp_get42 = ResultadoRepo(collection_name='resultados')
@@ -15,3 +16,7 @@ def find_all_resultados():
     df_gte42['cenario_wp'] = 'gte42'
 
     return pd.concat([df_full, df_gte42])
+
+def distribuicao_distancias(resultados, hue='cenario_wp'):
+    ax = sns.displot(resultados, x='distancia_destino', kind='hist', aspect=1.2, hue=hue, kde=True)
+    ax.set(xlabel='Distância entre documentos', ylabel='Frequência', title='Distribuição da distância entre documentos')
---
+++ src/python/notebooks/resultados/topico_dominante.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+def format_topics_sentences(ldamodel=None, corpus=None, texts=None):
+    # Init output
+    sent_topics_df = pd.DataFrame()
+
+    # Get main topic in each document
+    for i, row_list in enumerate(ldamodel[corpus]):
+        row = row_list[0] if ldamodel.per_word_topics else row_list            
+        row = sorted(row, key=lambda x: (x[1]), reverse=True)
+        # Get the Dominant topic, Perc Contribution and Keywords for each document
+        for j, (topic_num, prop_topic) in enumerate(row):
+            if j == 0:  # => dominant topic
+                wp = ldamodel.show_topic(topic_num)
+                topic_keywords = ", ".join([word for word, prop in wp])
+                sent_topics_df = sent_topics_df.append(pd.Series([int(topic_num), round(prop_topic,4), topic_keywords]), ignore_index=True)
+            else:
+                break
+    sent_topics_df.columns = ['Dominant_Topic', 'Perc_Contribution', 'Topic_Keywords']
+
+    # Add original text to the end of the output
+    contents = pd.Series(texts)
+    sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
+    return (sent_topics_df)
+
+def get_most_significant_sentences(df_topic_sents_keywords):
+    # Display setting to show more characters in column
+    pd.options.display.max_colwidth = 100
+
+    sent_topics_sorteddf_mallet = pd.DataFrame()
+    sent_topics_outdf_grpd = df_topic_sents_keywords.groupby('Dominant_Topic')
+
+    for i, grp in sent_topics_outdf_grpd:
+        sent_topics_sorteddf_mallet = pd.concat([sent_topics_sorteddf_mallet, 
+                                                grp.sort_values(['Perc_Contribution'], ascending=False).head(1)], 
+                                                axis=0)
+
+    # Reset Index    
+    sent_topics_sorteddf_mallet.reset_index(drop=True, inplace=True)
+
+    # Format
+    sent_topics_sorteddf_mallet.columns = ['Topic_Num', "Topic_Perc_Contrib", "Keywords", "Representative Text"]
+
+    return sent_topics_sorteddf_mallet
